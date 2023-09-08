d2d1cf01b5a9ff378a02ced9183f6b60899ab2ff
unknown <admin@ICT-PC1.trobank.com>
2023-09-08 12:56:33 +0300

    first portion of express app with middleware

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,13 @@ const express = require('express')
 // express app
 const intranet = express()
 
-//reacting to a request
+//middleware
+intranet.use((req, res, next) =>{
+    console.log(req.path, req.method)
+    next()
+})
+
+//reacting to a request. API END POINT
 intranet.get('/', (req,res) => {
     res.json({mssg: 'Welcome to the app'})
 })
